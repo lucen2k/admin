7c57ba329bcb77aff8fb4fd02975f04dee09842d
harukawa <harukawa@beyond-inc.jp>
2015-11-06 00:10:28 +0900

    初期コミット

---
+++ fuel/app_admin/logs/2015/11/05.php
@@ -0,0 +1,6 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2015-11-05 15:29:17 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2015-11-05 15:49:52 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2015-11-05 15:52:03 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2015-11-05 15:52:28 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
